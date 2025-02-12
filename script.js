f5b1968da5b927de11b1dc8564f08534341821d3
James Ivan Suminguit <jvansuminguit@gmail.com>
2025-02-13 04:21:24 +0800

    WIP - personalize content

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+  "Did you misclick or something bb?😅",
+  "Hmmm?🤔",
+  "Did you just say no???🥹",
+  "What?!?!? No wayyy😫",
+  "Whyyyyyy?😭",
+  "Bb please...🥺",
+  "Are you really sure bb???🥹",
+  "Last chance bb...🥺",
+  "LAST LAST CHANCE BB...🥺",
+  "I'm gonna be so sad bb☹️",
+  "You can't say no to me bb🥲",
+  "Sorry, but you're not getting out of this🤭",
+  "I'm not even gonna ask you again...🤷‍♂️",
 ];
 
 let messageIndex = 0;
 
 function handleNoClick() {
-    const noButton = document.querySelector('.no-button');
-    const yesButton = document.querySelector('.yes-button');
-    noButton.textContent = messages[messageIndex];
-    messageIndex = (messageIndex + 1) % messages.length;
-    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+  const noButton = document.querySelector(".no-button");
+  const yesButton = document.querySelector(".yes-button");
+  noButton.textContent = messages[messageIndex];
+  messageIndex = (messageIndex + 1) % messages.length;
+  const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+  yesButton.style.fontSize = `${currentSize * 1.5}px`;
+  if (messageIndex === messages.length - 1) {
+    window.location.href = "yes.html";
+  }
 }
 
 function handleYesClick() {
-    window.location.href = "yes_page.html";
+  window.location.href = "yes.html";
 }
