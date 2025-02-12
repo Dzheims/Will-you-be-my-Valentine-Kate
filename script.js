090f8c807ef707c0f94a29c6422bfaa5be78a8da
James Ivan Suminguit <jvansuminguit@gmail.com>
2025-02-13 04:52:15 +0800

    More personalization

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 const messages = [
-  "Did you misclick or something bb?😅",
-  "Hmmm?🤔",
-  "Did you just say no???🥹",
-  "What?!?!? No wayyy😫",
-  "Whyyyyyy?😭",
+  "Misclicked?😅",
+  "No???😱",
+  "Why not??🥲",
+  "Sure ka nagid???🥹",
   "Bb please...🥺",
-  "Are you really sure bb???🥹",
-  "Last chance bb...🥺",
-  "LAST LAST CHANCE BB...🥺",
-  "I'm gonna be so sad bb☹️",
-  "You can't say no to me bb🥲",
-  "Sorry, but you're not getting out of this🤭",
-  "I'm not even gonna ask you again...🤷‍♂️",
+  "Sure sure it's a no?🥺",
+  "LAST CHANCE BB...🥺",
+  "K",
+  "Joke! huhu",
+  "I'll be super sad🥲",
+  "Sge last nagid...",
+  "YES!!!😛",
+  "",
+  "",
 ];
 
 let messageIndex = 0;
 
 function handleNoClick() {
+  // Make the No button look like Yes button when showing "YES!!!😛"
+  if (messages[messageIndex - 1] === "YES!!!😛") {
+    window.location.href = "yes.html";
+  }
   const noButton = document.querySelector(".no-button");
   const yesButton = document.querySelector(".yes-button");
   noButton.textContent = messages[messageIndex];
   messageIndex = (messageIndex + 1) % messages.length;
   const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
   yesButton.style.fontSize = `${currentSize * 1.5}px`;
-  if (messageIndex === messages.length - 1) {
-    window.location.href = "yes.html";
+
+  if (messages[messageIndex - 1] === "YES!!!😛") {
+    noButton.style.backgroundColor = "#4caf50";
   }
 }
+
 
 function handleYesClick() {
   window.location.href = "yes.html";
